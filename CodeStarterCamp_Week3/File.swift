c737ecbae469e7dc951327f7cf8666b528bd7b83
CODEMASTERJO <ekrktk1221@naver.com>
2022-08-11 15:19:27 +0900

    feat: Struct person

--- CodeStarterCamp_Week3/File.swift
+++ CodeStarterCamp_Week3/File.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
+struct Person {
+    var name: String
+    var age: Int
+    var money: Int
+    
+    init(name: String, age: Int, money: Int) {
+        self.name = name
+        self.age = age
+        self.money = money
+    }
+        
+    func orderCoffee(haveMoney: Int, coffee: Coffee) {
+        if haveMoney >= coffee.coffeePrice {
+            print("""
+            \(coffee.coffeePrice)원입니다. 잔돈 \(haveMoney - coffee.coffeePrice)원 드리겠습니다.
+            \(coffee) 준비해 드리겠습니다. 픽업테이블에서 기다려주세요.
+            """)
+        } else {
+                print("잔액이 부족합니다.")
+        }
+    }
+}
 
 enum Coffee {
     case americano
