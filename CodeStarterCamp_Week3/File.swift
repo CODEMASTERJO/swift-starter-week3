e7ca7ed99db2fa2ad8085912d4b7e77bdd88bc79
CODEMASTERJO <ekrktk1221@naver.com>
2022-08-11 15:27:18 +0900

    feat: Class coffeeShop

--- CodeStarterCamp_Week3/File.swift
+++ CodeStarterCamp_Week3/File.swift
@@ -11,13 +11,11 @@ struct Person {
     var name: String
     var age: Int
     var money: Int
-    
     init(name: String, age: Int, money: Int) {
         self.name = name
         self.age = age
         self.money = money
     }
-        
     func orderCoffee(haveMoney: Int, coffee: Coffee) {
         if haveMoney >= coffee.coffeePrice {
             print("""
@@ -30,6 +28,23 @@ struct Person {
     }
 }
 
+class CoffeeShop {
+    var shopName: String
+    var totalSales: Int = 0
+    var menu: [String: Int]
+    var pickupTable : [Coffee]
+    var barista: Person
+    init(shopName: String, menu: [String: Int], pickupTable: [Coffee], barista: Person) {
+        self.shopName = shopName
+        self.menu = menu
+        self.pickupTable = pickupTable
+        self.barista = barista
+    }
+    func makeCoffee(name: String, coffee: Coffee) {
+        pickupTable.append(coffee)
+        print("\(name)님 주문하신 \(coffee)가 준비되었습니다. 픽업대에서 가져가주시기 바랍니다.")
+    }
+}
 enum Coffee {
     case americano
     case cafeLatte
